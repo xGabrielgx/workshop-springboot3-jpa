74d28f63ee93984fd482b3dbd750992db08727b3
Gabriel Xavier <gabrielgx928@gmail.com>
2024-09-12 15:16:56 -0300

    Subtotal & Total methods

--- src/main/java/com/projectspring/course/entities/Payment.java
+++ src/main/java/com/projectspring/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.projectspring.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,6 +21,7 @@ public class Payment implements Serializable {
 
 
     // associacao 1 para 1 (coloca na classe que é dependente)
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
