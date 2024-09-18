f1c2b40bd81e8e5f569efb936e89e75be507f1ea
Gabriel Xavier <gabrielgx928@gmail.com>
2024-09-18 15:50:54 -0300

    Exception handling - findById

---
+++ src/main/java/com/projectspring/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.projectspring.course.resources.exceptions;
+
+import com.projectspring.course.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+// intercepta as exceções que acontecem para que esse objeto trate a exceção
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    // anotation para intercepta a excessão
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/projectspring/course/services/UserService.java
+++ src/main/java/com/projectspring/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.projectspring.course.services;
 
 import com.projectspring.course.entities.User;
 import com.projectspring.course.repositories.UserRepository;
+import com.projectspring.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +24,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
